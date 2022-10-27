54b58d469720424620b8060823a16fdcd9e7a2ae
Sil van Diepen <me@sil.mt>
2022-10-27 14:26:41 +0200

    build: added

--- dist/helpers.js
+++ dist/helpers.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.toKebabCase = void 0;
-const toKebabCase = (str) => str
-    .replace(/([a-z])([A-Z])/g, "$1-$2")
-    .replace(/[\s_]+/g, "-")
-    .toLowerCase();
+const toKebabCase = (str) => str !== ""
+    ? str
+        .replace(/([a-z])([A-Z])/g, "$1-$2")
+        .replace(/[\s_]+/g, "-")
+        .toLowerCase()
+    : "";
 exports.toKebabCase = toKebabCase;
